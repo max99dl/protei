f5f085cb3d822d9c86dc662c0ceea2957dc99692
max99dl <99542366+max99dl@users.noreply.github.com>
2023-12-04 15:32:35 +0700

    Update assemble.sh
    will assemble without main CMakeLists.txt

--- protei/assemble.sh
+++ protei/assemble.sh
@@ -2,6 +2,7 @@
 
 mkdir server;
 mv server_config.json server;
+cp tests/test_server.cpp src/
 
 cd libraries;
 mkdir build;
@@ -9,17 +10,19 @@ cd build;
 cmake .. -DCMAKE_INSTALL_PREFIX=install;
 cmake --build .;
 cmake --build . --target install;
+cp install/lib/libweb_server_lib.a ../../src;
+cp install/include/web_server.hpp ../../src;
 
 cd ../../tests/googletest;
 cmake -Bbuild;
 cmake --build build;
+cp ./build/lib/lib* ../../src/;
 
-cd ../../;
-mkdir build;
-cd build;
-cmake .. -Dweb_server_lib_DIR=./libraries/build/install/lib/cmake;
-cmake --build .;
-mv main ./../server;
-mv tests ./../server;
+cd ../../src;
+g++ -o tests -pthread -std=c++17 test_server.cpp -L. libweb_server_lib.a libgtest.a libgtest_main.a -I"../libraries/boost" -I"../libraries/spdlog/include" -I"../tests/googletest/include";
+g++ -o main -pthread -std=c++17 main.cpp -L. libweb_server_lib.a -I"../libraries/spdlog/include" -I"../libraries/boost";
+mv tests ../server;
+mv main ../server;
 
+rm test_server.cpp lib* web_server.hpp;
 cd ..;
